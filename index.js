3f34a4d5674e01dc36def5e1abff797778215b97
Adam Kearney <aklives@hotmail.com>
2019-03-08 20:41:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function createRock(x) {
   // Hmmm, why would we have used `var` here?
    var top2 = rock.style.top = 0;
 
-  rock.style.top = top;
+  
 
   /**
    * Now that we have a rock, we'll need to append
